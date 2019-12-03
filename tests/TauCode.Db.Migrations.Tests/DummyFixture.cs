96dff6ebc67ea0dcbca0ac064b663c5fef1744b4
Andrey Kovalenko <ak@taucode.com>
2019-12-03 23:29:24 +0200

    Added ci for migrations proj.

---
+++ tests/TauCode.Db.Migrations.Tests/DummyFixture.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace TauCode.Db.Migrations.Tests
+{
+    [TestFixture]
+    public class DummyFixture
+    {
+        [Test]
+        public void DummyTest()
+        {
+            Assert.Pass("Added for Jenkins.");
+        }
+    }
+}
