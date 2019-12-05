feaed50b5e6f3c9551074c2f9a37f1dc83be6cd9
Andrey Kovalenko <ak@taucode.com>
2019-12-05 22:54:33 +0200

    Added stubs for ci.

---
+++ tests/TauCode.Db.Testing.Tests/DbTestingDummyFixture.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace TauCode.Db.Testing.Tests
+{
+    // todo remove
+    [TestFixture]
+    public class DbTestingDummyFixture
+    {
+        [Test]
+        public void DbTestingDummyTest()
+        {
+            Assert.Pass("Added for Jenkins.");
+        }
+    }
+}
---
+++ tests/TauCode.Db.Tests.SQLite/DbSQLiteDummyFixture.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace TauCode.Db.Tests.SQLite
+{
+    // todo remove
+    [TestFixture]
+    public class DbSQLiteDummyFixture
+    {
+        [Test]
+        public void DbSQLiteDummyTest()
+        {
+            Assert.Pass("Added for Jenkins.");
+        }
+    }
+}
