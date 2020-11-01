4b77e6128c4f5f61ffeeed2cbc5d70b424a45d8f
Andrey Kovalenko <ak@taucode.com>
2020-11-01 20:10:56 +0200

    NpgsqlInspectorTestsLab ut-s green.

---
+++ taucode-lab/TauCode.Lab.Db.Npgsql/NpgsqlTableInspectorLab.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using TauCode.Db;
+using TauCode.Db.Model;
+
+namespace TauCode.Lab.Db.Npgsql
+{
+    public class NpgsqlTableInspectorLab : DbTableInspectorBase
+    {
+        public NpgsqlTableInspectorLab(IDbConnection connection, string schemaName, string tableName) : base(connection, schemaName, tableName)
+        {
+        }
+
+        public override IDbUtilityFactory Factory => throw new NotImplementedException();
+        protected override List<ColumnInfo> GetColumnInfos()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override ColumnMold ColumnInfoToColumnMold(ColumnInfo columnInfo)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override Dictionary<string, ColumnIdentityMold> GetIdentities()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override bool NeedCheckSchemaExistence => throw new NotImplementedException();
+        protected override bool SchemaExists(string schemaName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override bool TableExists(string tableName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override PrimaryKeyMold GetPrimaryKeyImpl()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override IReadOnlyList<ForeignKeyMold> GetForeignKeysImpl()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override IReadOnlyList<IndexMold> GetIndexesImpl()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ taucode-lab/TauCode.Lab.Db.Npgsql/NpgsqlUtilityFactoryLab.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Data;
+using TauCode.Db;
+
+namespace TauCode.Lab.Db.Npgsql
+{
+    public class NpgsqlUtilityFactoryLab : IDbUtilityFactory
+    {
+        public static NpgsqlUtilityFactoryLab Instance { get; } = new NpgsqlUtilityFactoryLab();
+
+        private NpgsqlUtilityFactoryLab()
+        {   
+        }
+
+        public IDbDialect GetDialect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDbScriptBuilder CreateScriptBuilder(string schemaName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDbConnection CreateConnection()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDbInspector CreateInspector(IDbConnection connection, string schemaName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDbTableInspector CreateTableInspector(IDbConnection connection, string schemaName, string tableName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDbCruder CreateCruder(IDbConnection connection, string schemaName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDbSerializer CreateSerializer(IDbConnection connection, string schemaName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
