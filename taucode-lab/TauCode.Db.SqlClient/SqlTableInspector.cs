2c0cdccee2a22f5030e2ae1f0c0be5d487896360
Andrey Kovalenko <ak@taucode.com>
2020-11-12 02:26:38 +0200

    Migrated SQL server.

---
+++ taucode-lab/TauCode.Db.SqlClient/SqlTableInspector.cs
@@ -0,0 +1,22 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+using TauCode.Db.Schema;
+
+namespace TauCode.Db.SqlClient
+{
+    public class SqlTableInspector : DbTableInspectorBase
+    {
+        public SqlTableInspector(SqlConnection connection, string schemaName, string tableName)
+            : base(
+                connection,
+                schemaName ?? SqlTools.DefaultSchemaName,
+                tableName)
+        {
+        }
+
+        public override IDbUtilityFactory Factory => SqlUtilityFactory.Instance;
+
+        protected override IDbSchemaExplorer CreateSchemaExplorer(IDbConnection connection) =>
+            new SqlSchemaExplorer((SqlConnection) this.Connection);
+    }
+}
--- taucode-lab/TauCode.Lab.Db.SqlClient/SqlTools.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TauCode.Lab.Db.SqlClient
-{
-    // todo: consider rename to 'SqlConstants'
-    public static class SqlTools
-    {
-        public const string DefaultSchemaName = "dbo";
-    }
-}
