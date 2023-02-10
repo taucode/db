c8a9a0f2251ec1dc525a2f81de8ecac8dde7f36b
Andrey Kovalenko <ak@taucode.com>
2023-02-10 15:49:50 +0200

    DbTypeMold ported.

---
+++ src/TauCode.Db/Model/Interfaces/IDbTypeMold.cs
@@ -0,0 +1,10 @@
+﻿namespace TauCode.Db.Model.Interfaces;
+
+public interface IDbTypeMold : INamedMold
+{
+    public int? Size { get; set; }
+
+    public int? Precision { get; set; }
+
+    public int? Scale { get; set; }
+}
