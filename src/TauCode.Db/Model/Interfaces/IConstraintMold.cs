c4f239109735c4d83eb6b0af9b1904aba59f27e3
Andrey Kovalenko <ak@taucode.com>
2023-02-10 16:55:31 +0200

    columnMold ported.

--- src/TauCode.Db/Model/Interfaces/IConstraintMold.cs
+++ src/TauCode.Db/Model/Interfaces/IConstraintMold.cs
@@ -4,5 +4,7 @@ namespace TauCode.Db.Model.Interfaces;
 
 public interface IConstraintMold : INamedMold
 {
+    string? SchemaName { get; set; }
+    string TableName { get; set; }
     ConstraintType Type { get; }
 }
